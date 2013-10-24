cd4027346b0780a489a714a1b0a03ac5c3cae003
dnim <dnimic@gmail.com>
2013-10-25 01:28:26 +0300

    Initialize application. TODO: clean up (remove dependency)

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var expresshandlebars = require('express3-handlebars');
+
+var app = express();
+
+app.engine('handlebars', expresshandlebars({defaultLayout:'main'}));
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+//app.set('view engine', 'jade');
+app.set('view engine', 'handlebars');
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
