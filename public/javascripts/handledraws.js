0bf8681775067f0033e7dcc7297ec123dafdd975
dnim <dnimic@gmail.com>
2013-11-13 02:56:26 +0300

    Added rendering of a new draw entery. TODO: save draw in db from ui; render added draws

--- public/javascripts/handledraws.js
+++ public/javascripts/handledraws.js
@@ -1,15 +1,29 @@
-var colorChangeButtons = document.querySelectorAll(".btn.color-changer") 
-for (var i = 0; i < colorChangeButtons.length; i++) {
-	colorChangeButtons[i].onclick = function (event) {
-		var cl = this.classList;
-		if(cl.contains("btn-warning")){
-			cl.remove("btn-warning");
-			cl.add("btn-black");
-			this.parentNode.children[1].value = "black"
-		} else {
-			cl.remove("btn-black");
-			cl.add("btn-warning");
-			this.parentNode.children[1].value = "yellow"
-		}
-	}
-};
+$(document).ready(function () {
+  var newDrawTemplate = Handlebars.compile($("#ball-form").html());
+  var renderOptions = Handlebars.registerHelper("renderOptions", function () {
+    var out = ""; 
+    for(var i = 0; i < 100; i++) out+= "<option value=\"" + i + "\">" + i + "</option>";
+    return out;
+  });
+  var drawForm = "";
+  for (var j = 1; j < 7 ; j++ ) drawForm += newDrawTemplate( {index:j} );
+  // document.getElementById("add-new-draw").html( newDrawTemplate( {index:1} ) );
+  $("#add-new-draw").html( drawForm );
+
+  var colorChangeButtons = document.querySelectorAll(".btn.color-changer") 
+  for (var i = 0; i < colorChangeButtons.length; i++) {
+    colorChangeButtons[i].onclick = function (event) {
+      var cl = this.classList;
+      if(cl.contains("btn-warning")){
+        cl.remove("btn-warning");
+        cl.add("btn-black");
+        this.parentNode.children[1].value = "black"
+      } else {
+        cl.remove("btn-black");
+        cl.add("btn-warning");
+        this.parentNode.children[1].value = "yellow"
+      }
+    }
+  };
+});
+
