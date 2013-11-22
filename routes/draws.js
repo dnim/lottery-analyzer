7979d001493b652d18b055de3d69de7083b24ab6
dnim <dnimic@gmail.com>
2013-11-22 20:42:19 +0300

    Added css; transferred js file

--- routes/draws.js
+++ routes/draws.js
@@ -1,8 +1,11 @@
 // TODO: add logic to handle draws / statistics and so on
 
 exports.newdraw = function (db){
+  // TODO: add to the db new entry, render it on the UI
   return function (req, res) {
-    res.render("BOOM!");
+    res.render("draws", {
+      requestedData: req.body["ball-1"]
+    });
   }
 };
 
